15eacfe3927213ef0cc9e8d7733ba994d72b4b93
Danti0ch <ivanov.mikhail@phystech.edu>
2022-03-26 12:14:42 +0300

    changed identands + deleted trash

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
-extern "C" int prinft(const char* str, ...);
-//#include <stdio.h>
-
-//extern "C" int sum(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9);
-
-//int sum(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9){
-//	if(a1 != 0) int c = sum(0, 2, 3, 4, 5, 6, 7, 8, 9);
-//	return a1 + a9;
-//}
-
 #include <stdio.h>
+extern "C" int prinft(const char* str, ...);
 
 int main(){
-	prinft("%d %d %d %d %d %d %d %d %d %d %d\n"
-	       "I %s %x %d%%%c%b",
-		 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-		"LOVE", 3802, 100, 33, 15);
-	//prinft("%d, %d", 1, 2);
-	//sum(1, 2, 3, 4, 5, 6, 7, 8, 9);
-	return 0;
+    prinft("%d %d %d %d %d %d %d %d %d %d %d\n"
+           "I %s %x %d%%%c%b",
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                "LOVE", 3802, 100, 33, 15);
+    return 0;
 }
--- prinft.s
+++ prinft.s
@@ -1,341 +1,333 @@
 
 section .data
+
 test_string: db "ahahah, %d %d %d %d %d %d %s %b", 0x00
 msg_test:    db "stavim na zero", 0X00
-string1:     db "LOVE", 0x00
 ;__________________________________
 section .rodata
 
 PROCENT_SYMB     equ '%'
 END_STRING_VALUE equ 0X00
-STDOUT_DESCR	 equ 1
+STDOUT_DESCR     equ 1
 alphabet: db "0123456789abcdefghi"
 
 SYSCALL_EXIT equ 0x3c
 SYSCALL_OUTPUT equ 0x01
 ;__________________________________
 section .bss
+
 temp_string: resw 0x100
 ;__________________________________
 section .text
 
 global prinft
 ;global _start
+
 ;_start:
-;	mov rdi, test_string
-;	mov rsi, 0x01
-;	mov rdx, 0x02
-;	mov rcx, 0x03
-;	mov r8,  0x04
-;	mov r9,  0x05
-;	push 0x10
-;	push msg_test
-;;	push 0x10
-;	call prinft
+;   mov rdi, test_string
+;   mov rsi, 0x01
+;   mov rdx, 0x02
+;   mov rcx, 0x03
+;   mov r8,  0x04
+;   mov r9,  0x05
+;   push 0x10
+;   push msg_test
+;   push 0x10
+;   call prinft
 ;
-;	add rsp, 8*7 + 5*8
+;   add rsp, 8*7 + 5*8
 ;
-;	mov rax, SYSCALL_EXIT
-;	xor rdi, rdi
-;	syscall
+;   mov rax, SYSCALL_EXIT
+;   xor rdi, rdi
+;   syscall
 
 ;===============================================================
 ; model: cdecl
 ; args:
-;	1) string pointer
+;   1) string pointer
 ;       2-inf) parms for string
 ; ret: 
-;	rax - number of characters output
+;   rax - number of characters output
 ;==============================================================
 prinft:
-	push rbp
-	mov rbp, rsp
-	
-	sub rsp, 0x30
-	mov r10, rdi
-	mov qword [rbp - 0x10], rsi
-	mov qword [rbp - 0x18], rdx
-	mov qword [rbp - 0x20], rcx
-	mov qword [rbp - 0x28], r8
-	mov qword [rbp - 0x30], r9
+    push rbp
+    mov rbp, rsp
+    
+    sub rsp, 0x30
+    mov r10, rdi
+    mov qword [rbp - 0x10], rsi
+    mov qword [rbp - 0x18], rdx
+    mov qword [rbp - 0x20], rcx
+    mov qword [rbp - 0x28], r8
+    mov qword [rbp - 0x30], r9
 
-	mov rsi, r10
-	mov rdi, temp_string					; указатель на строку, в которую мы будем ложить все данные
-	
-	xor rcx, rcx							; args_readen = 0
+    mov rsi, r10
+    mov rdi, temp_string                    ; указатель на строку, в которую мы будем ложить все данные
+    
+    xor rcx, rcx                            ; args_readen = 0
 string_conv_ph1:
-	cmp rcx, 0x05
-	je string_conv_ph2
+    cmp rcx, 0x05
+    je string_conv_ph2
 
-	cmp byte [rsi], END_STRING_VALUE
-	je after_converting
+    cmp byte [rsi], END_STRING_VALUE
+    je after_converting
 
-	cmp byte [rsi], PROCENT_SYMB
-	je spec_handler1
-	movsb
-	
-	jmp string_conv_ph1
+    cmp byte [rsi], PROCENT_SYMB
+    je spec_handler1
+    movsb
+    
+    jmp string_conv_ph1
 
 spec_handler1:
-	inc rsi
-	cmp byte [rsi], PROCENT_SYMB
-	jne not_percent_spec1
-	mov byte [rdi], PROCENT_SYMB
-	inc rsi
-	inc rdi
-	jmp string_conv_ph1
+    inc rsi
+    cmp byte [rsi], PROCENT_SYMB
+    jne not_percent_spec1
+    mov byte [rdi], PROCENT_SYMB
+    inc rsi
+    inc rdi
+    jmp string_conv_ph1
 not_percent_spec1:
-	
-	mov rax, rcx
-	shl rax, 3
-	add rax, 0x10
-	mov rdx, rbp
-	sub rdx, rax
+    
+    mov rax, rcx
+    shl rax, 3
+    add rax, 0x10
+    mov rdx, rbp
+    sub rdx, rax
 
-	mov rax, [rdx]
+    mov rax, [rdx]
 
-	xor rdx, rdx
-	mov dl, byte [rsi]						; dl = ascii of symb after %
-	shl rdx, 3
+    xor rdx, rdx
+    mov dl, byte [rsi]                      ; dl = ascii of symb after %
+    shl rdx, 3
 
-	call [rdx+jump_table]			; вызываем обработку спецификатора по свичу
-	
-	inc rsi
-	inc rcx									; args_readen++
-	
-	jmp string_conv_ph1
+    call [rdx+jump_table]                   ; вызываем обработку спецификатора по свичу
+    
+    inc rsi
+    inc rcx                                 ; args_readen++
+    
+    jmp string_conv_ph1
 string_conv_ph2:
 
-	cmp byte [rsi], END_STRING_VALUE
-	je after_converting
+    cmp byte [rsi], END_STRING_VALUE
+    je after_converting
 
-	cmp byte [rsi], PROCENT_SYMB
-	je spec_handler2
-	movsb
-	
-	jmp string_conv_ph2
+    cmp byte [rsi], PROCENT_SYMB
+    je spec_handler2
+    movsb
+    
+    jmp string_conv_ph2
 
 spec_handler2:
-	inc rsi
-	cmp byte [rsi], PROCENT_SYMB
-	jne not_percent_spec2
-	mov byte [rdi], PROCENT_SYMB
-	inc rsi
-	inc rdi
-	jmp string_conv_ph2
+    inc rsi
+    cmp byte [rsi], PROCENT_SYMB
+    jne not_percent_spec2
+    mov byte [rdi], PROCENT_SYMB
+    inc rsi
+    inc rdi
+    jmp string_conv_ph2
 not_percent_spec2:
-		
-	xor rdx, rdx
-	mov dl, byte [rsi]						; dl = ascii of symb after %
-	shl rdx, 3
-	
-	mov rax, [rbp + rcx * 0x08 - 0x28 + 0x10]				; rax = arg_value
-	
-	call [rdx + jump_table]			; вызываем обработку спецификатора по свичу
-	
-	inc rsi
-	inc rcx									; args_readen++
-	
-	cmp byte [rsi], END_STRING_VALUE
-	jne string_conv_ph2
+        
+    xor rdx, rdx
+    mov dl, byte [rsi]                              ; dl = ascii of symb after %
+    shl rdx, 3
+    
+    mov rax, [rbp + rcx * 0x08 - 0x28 + 0x10]       ; rax = arg_value
+    
+    call [rdx + jump_table]                         ; вызываем обработку спецификатора по свичу
+    
+    inc rsi
+    inc rcx                                         ; args were read++
+    
+    cmp byte [rsi], END_STRING_VALUE
+    jne string_conv_ph2
 
 after_converting:
-	push temp_string
-	call strlen
+    push temp_string
+    call strlen
 
-	mov rdx, rax
-	mov rax, SYSCALL_OUTPUT
-	mov rdi, STDOUT_DESCR
-	mov rsi, temp_string
-	syscall									; print(temp_string)
-	mov rax, 0x01
+    mov rdx, rax
+    mov rax, SYSCALL_OUTPUT
+    mov rdi, STDOUT_DESCR
+    mov rsi, temp_string
+    syscall                                 ; print(temp_string)
+    mov rax, 0x01
 
-	add rsp, 0x30
-	pop rbp	
-	ret
+    add rsp, 0x30
+    pop rbp 
+    ret
 ;===============================================================
 ; ФУНКЦИИ - ОБРАБОТЧКИ СПЕЦИФИКАТОРОВ (CASES)
 ;================================================================
 default_spec:
-	mov byte [rdi], PROCENT_SYMB
-	inc rdi
+    mov byte [rdi], PROCENT_SYMB
+    inc rdi
 
-	movsb
-	dec rsi
-	ret
+    movsb
+    dec rsi
+    ret
 
 int2_spec:
-	push 0x02
-	push rdi
-	push rax
-	call itoa
-	
-	ret
+    push 0x02
+    push rdi
+    push rax
+    call itoa
+    
+    ret
 
 char_spec:
-	mov byte [rdi], al
-	inc rdi
+    mov byte [rdi], al
+    inc rdi
 
-	ret
+    ret
 
 int10_spec:
-	push 0x0a
-	push rdi
-	push rax
-	call itoa
-	
-	ret
+    push 0x0a
+    push rdi
+    push rax
+    call itoa
+    
+    ret
 
 int8_spec:
-	push 0x08
-	push rdi
-	push rax
-	call itoa
-	
-	ret
+    push 0x08
+    push rdi
+    push rax
+    call itoa
+    
+    ret
 
 string_spec:
-	push rsi
-	mov rsi, rax
+    push rsi
+    mov rsi, rax
 
 s_spec_loop:
-	movsb
-	cmp byte [rsi], 0x00
-	jne s_spec_loop
-	
-	pop rsi
-	
-	ret
+    movsb
+    cmp byte [rsi], 0x00
+    jne s_spec_loop
+    
+    pop rsi
+    
+    ret
 
 int16_spec:
-	push 0x10
-	push rdi
-	push rax
-	call itoa
-	
-	ret
+    push 0x10
+    push rdi
+    push rax
+    call itoa
+    
+    ret
 ;__________________________________
 section .rodata
 
-;specif_cases:
-;	dq default_spec
-;	dq int2_spec
-;	dq int16_spec
-;	dq char_spec
-;	dq int10_spec
-;	dq int8_spec
-;	dq string_spec
-
 jump_table: 
-		times 'b' 		dq 0x00
-						dq int2_spec
-						dq char_spec
-						dq int10_spec
-		times 'o' - 'd' - 1 	dq 0x00
-						dq int8_spec
-		times 's' - 'o' - 1	dq 0x00
-						dq string_spec
-		times 'x' - 's' - 1	dq 0x00
-						dq int16_spec
-		times 0x100 - 'x' dq 0x00
+        times 'b'       dq 0x00
+                        dq int2_spec
+                        dq char_spec
+                        dq int10_spec
+        times 'o' - 'd' - 1     dq 0x00
+                        dq int8_spec
+        times 's' - 'o' - 1 dq 0x00
+                        dq string_spec
+        times 'x' - 's' - 1 dq 0x00
+                        dq int16_spec
+        times 0x100 - 'x' dq 0x00
 section .text
 ;===============================================================
 ; model: stdcall
 ; args:
-;	1) string pointer
+;   1) string pointer
 ; ret: 
-;	rax - string length
+;   rax - string length
 ;==============================================================
 strlen:
 
-    	push rbp
-    	mov rbp, rsp
-	
-    	mov rdi, qword [rbp + 16]
-	
-	xor al, al				; end of the string value
-	mov rdx, rdi				; saving base offset
+    push rbp
+    mov rbp, rsp
 
-loop_until_end_symb:	
-	inc rdi
-	cmp al, byte [rdi]
-	jne loop_until_end_symb
+    mov rdi, qword [rbp + 16]
 
-    	sub rdi, rdx
-    	mov rax, rdi
+    xor al, al                              ; end of the string value
+    mov rdx, rdi                            ; saving base offset
 
-	pop rbp
-	ret 8
+loop_until_end_symb:    
+    inc rdi
+    cmp al, byte [rdi]
+    jne loop_until_end_symb
+
+    sub rdi, rdx
+    mov rax, rdi
+
+    pop rbp
+    ret 8
 
 ;===================================================
 ; model: stdcall
 ; args:
-;	1) value
-;	2) pointer of the string to write
-;	3) the basis of the calculus system
+;   1) value
+;   2) pointer of the string to write
+;   3) the basis of the calculus system
 ;
 ; ret:
-;	di - pointer of the string to write
+;   rdi - pointer of the string to write
 ;===================================================
 itoa:
-	push rbp
-	mov rbp, rsp
-	
-	push rsi
-	push rcx
+    push rbp
+    mov rbp, rsp
+    
+    push rsi
+    push rcx
 
-	mov rax, qword [rbp + 16]
-	mov rdi, qword [rbp + 24]
-	mov rcx, qword [rbp + 32]
-	mov rbx, alphabet
-	
-	mov rsi, rdi	
+    mov rax, qword [rbp + 16]
+    mov rdi, qword [rbp + 24]
+    mov rcx, qword [rbp + 32]
+    mov rbx, alphabet
+    
+    mov rsi, rdi    
 default_converting_to_string:
-	
-	xor rdx, rdx	
-	div rcx
-	
-	push rax
-	mov rax, rbx
-	add rax, rdx
-	
-	mov al, byte [rax]
-	stosb
-	
-	pop rax
-	cmp rax, 0x00
-	jne default_converting_to_string
-	
-	jmp init_reverse
+    
+    xor rdx, rdx    
+    div rcx
+    
+    push rax
+    mov rax, rbx
+    add rax, rdx
+    
+    mov al, byte [rax]
+    stosb
+    
+    pop rax
+    cmp rax, 0x00
+    jne default_converting_to_string
+    
+    jmp init_reverse
 
 init_reverse:
-	
-	mov al, 0x00
-	mov byte [rdi], al
+    
+    mov al, 0x00
+    mov byte [rdi], al
 
-	mov rdx, rsi				; save pointer to the string
-	
-	mov rcx, rdi
-	sub rcx, rsi				; rcx = strlen = rdi - rsi
-	shr rcx, 1				; rcx = strlen/2
-	
-	jz after_reverse
-	push rdi
-	dec rdi
+    mov rdx, rsi                            ; save pointer to the string
+    
+    mov rcx, rdi
+    sub rcx, rsi                            ; rcx = strlen = rdi - rsi
+    shr rcx, 1                              ; rcx = strlen/2
+    
+    jz after_reverse
+    push rdi
+    dec rdi
 reversing:
 
-	mov al, byte [rdi]
-	xchg al, byte [rsi]
-	mov byte [rdi], al
-	
-	inc rsi
-	dec rdi
-	loop reversing
+    mov al, byte [rdi]
+    xchg al, byte [rsi]
+    mov byte [rdi], al
+    
+    inc rsi
+    dec rdi
+    loop reversing
 
-	pop rdi
+    pop rdi
 after_reverse:
-	
-	pop rcx	
-	pop rsi
-	pop rbp
-    	ret 24
+    
+    pop rcx 
+    pop rsi
+    pop rbp
